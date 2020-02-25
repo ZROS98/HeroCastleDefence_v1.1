43f8e71bb57ad4ad776c4000232b3269e80926b9
ZROS <zros98@gmail.com>
2020-02-25 22:10:27 +0100

    Add Fireball

---
+++ Assets/Code/Characters/ShootingMechanic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Resources;
+using UnityEngine;
+
+public class ShootingMechanic : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _projectile;
+    [SerializeField] private float _projectileSpeed = 20f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Rigidbody clone = Instantiate(_projectile, transform.position, transform.rotation);
+            clone.velocity = transform.TransformDirection(Vector3.forward * _projectileSpeed);
+        }
+    }
+}
---
+++ Assets/Code/Weapons/Fireball.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField] private GameObject _explosionVFX;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (_explosionVFX != null)
+        {
+            Instantiate(_explosionVFX, transform.position, transform.rotation);
+        }
+    }
+
+    private void Start()
+    {
+        Destroy(gameObject,10f);
+    }
+}
