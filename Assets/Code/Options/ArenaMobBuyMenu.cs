6f68a71b644b6f8dc9daf0b46ac39838de9a2d4d
ZROS <zros98@gmail.com>
2020-02-26 00:17:02 +0100

    Add options menu and BuyMob Menu to Arena + ambient

---
+++ Assets/Code/Options/ArenaMobBuyMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaMobBuyMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameObject;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B) && _gameObject != null) 
+        {
+            bool isActive = _gameObject.activeSelf;
+            _gameObject.SetActive(!isActive);
+        }
+    }
+}
---
+++ Assets/Code/Options/ArenaOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaOptions : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameObject;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && _gameObject != null) 
+        {
+            bool isActive = _gameObject.activeSelf;
+            Screen.lockCursor = isActive;
+            _gameObject.SetActive(!isActive);
+        }
+    }
+}
