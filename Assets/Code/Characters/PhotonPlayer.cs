9d46237afe33a0bdbab9938dee84e930be840b58
Elenger <orion96king@gmail.com>
2020-04-24 23:43:39 +0200

    some fixes

--- Assets/Code/Characters/PhotonPlayer.cs
+++ Assets/Code/Characters/PhotonPlayer.cs
@@ -11,17 +11,22 @@ public class PhotonPlayer : MonoBehaviourPunCallbacks
     //private PhotonView _PV;
     private GameObject _photonAvatar;
 
-    
+
+    public void Awake()
+    {
+         PhotonNetwork.Instantiate("Empty",
+                gameObject.transform.position, gameObject.transform.rotation, 0);
+
+    }
     public void CreateCharacter(GameObject myAvatar)
     {
-       
         //_PV = GetComponent<PhotonView>();
         int spawnPicker = Random.Range(0, GameSetup.GS.spawnPoints.Length);
         //if (_PV.IsMine)
         {
-            _photonAvatar = PhotonNetwork.Instantiate(myAvatar.name,
-                GameSetup.GS.spawnPoints[spawnPicker].position, GameSetup.GS.spawnPoints[spawnPicker].rotation, 0);
-        }
+            //_photonAvatar = PhotonNetwork.Instantiate(myAvatar.name,
+            //    GameSetup.GS.spawnPoints[spawnPicker].position, GameSetup.GS.spawnPoints[spawnPicker].rotation, 0);
+       }
         _photonAvatar.GetComponent<Movement>().chrCamera = mainCamera;
         gameObject.GetComponent<TurnOffPlayerControl>().character = _photonAvatar;
     }
