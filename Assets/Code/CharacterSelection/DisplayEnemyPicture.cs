3774ba91d64544a0e750b36856a5cd680e92065b
Elenger <orion96king@gmail.com>
2020-05-23 23:35:28 +0200

    Fix enemyAvatar on CharacterSelection.unity scene

--- Assets/Code/CharacterSelection/DisplayEnemyPicture.cs
+++ Assets/Code/CharacterSelection/DisplayEnemyPicture.cs
@@ -7,31 +7,34 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class DisplayEnemyPicture : MonoBehaviour
 {
+    [SerializeField] Sprite[] _avatarsPictureArray;
     [SerializeField] private PhotonView _photonView;
     private Hashtable _hash = new Hashtable();
     [SerializeField] private Image _imageForEnemyAvatar;
-    public Sprite myAvatarForEnemyPlayer;
+    public int myAvatarNumberForEnemyPlayer;
+
     private void Start()
     {
-        _hash.Add("EnemyPictureProperties", myAvatarForEnemyPlayer);
+        _hash.Add("AvatarNumberForEnemyPictureProperties", myAvatarNumberForEnemyPlayer);
     }
 
-    public void SetMyAvatarPictures(Sprite sprite)
+    public void SetMyAvatarPicturesNumber(int avatarNumber)
     {
-        _imageForEnemyAvatar.sprite = sprite;
-        SetForEnemyAvatarsPicture();
+        myAvatarNumberForEnemyPlayer = avatarNumber;
+        SetAvatarNumberForEnemyPictureProperties();
     }
 
-    private void SetForEnemyAvatarsPicture()
+    private void SetAvatarNumberForEnemyPictureProperties()
     {
-        _hash["EnemyPictureProperties"] = myAvatarForEnemyPlayer;
+        _hash["AvatarNumberForEnemyPictureProperties"] = myAvatarNumberForEnemyPlayer;
         PhotonNetwork.SetPlayerCustomProperties(_hash);
-        _photonView.RPC("SetForEnemyAvatarsPictureRPC", RpcTarget.OthersBuffered);
+        _photonView.RPC("SetAvatarNumberForEnemyPicturePropertiesRPC", RpcTarget.Others);
     }
 
     [PunRPC]
-    private void SetForEnemyAvatarsPictureRPC()
+    private void SetAvatarNumberForEnemyPicturePropertiesRPC()
     {
-        _imageForEnemyAvatar.sprite = (Sprite)PhotonNetwork.PlayerListOthers[0].CustomProperties["EnemyPictureProperties"];
+        int avatarNumber = (int) PhotonNetwork.PlayerListOthers[0].CustomProperties["AvatarNumberForEnemyPictureProperties"];
+        _imageForEnemyAvatar.sprite = _avatarsPictureArray[avatarNumber];
     }
 }
