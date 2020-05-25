fae03d62db5f039b3e19c5f2ea1b91c61c3f47f9
ZROS <zros98@gmail.com>
2020-05-25 20:44:48 +0200

    Add new character movement

---
+++ Assets/Code/Characters/ThirdPersonMovement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private Transform _camera;
+
+    private float speed = 6f;
+    private float turnSmoothTime = 0.0f;
+    private float turnSmoothVelocity;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+        Vector3 direction = new Vector3(horizontal,0f,vertical).normalized;
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,
+                turnSmoothTime);
+            transform.rotation= Quaternion.Euler(0f,targetAngle,0f);
+
+            Vector3 movementDirection = Quaternion.Euler(0f, targetAngle,0f)*Vector3.forward;
+            _characterController.Move(movementDirection.normalized * speed * Time.deltaTime);
+        }
+    }
+}
