18239ad8e66e4c3862ba9274bdfc6f417f0de11c
Elenger <orion96king@gmail.com>
2020-06-15 23:58:53 +0200

    work on MobAttack

--- Assets/Code/Mobs/MobAttack.cs
+++ Assets/Code/Mobs/MobAttack.cs
@@ -1,18 +1,24 @@
-﻿using System;
+﻿using Photon.Pun;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MobAttack : MonoBehaviour
 {
-    [SerializeField] private Weapon _weapon; 
+    [SerializeField] private Weapon _weapon;
+    [SerializeField] private PhotonView _photonView;
     public GameObject targetCharacter;
     public Animator animator;
+
+    private void Awake()
+    {
+        if (!_photonView.IsMine) enabled = false;
+    }
 
     private void Start()
     {
         animator = targetCharacter.GetComponent<Animator>();
-        
     }
 
     void Update()
