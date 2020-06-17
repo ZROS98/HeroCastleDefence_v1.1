92abcd0a4d9f99ee470f60f08fa5dfc05f177950
Elenger <orion96king@gmail.com>
2020-06-17 22:09:29 +0200

    Fix CharacterWeaponTransform

--- Assets/Code/Characters/CharacterWeaponTransform.cs
+++ Assets/Code/Characters/CharacterWeaponTransform.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterAnimations : MonoBehaviour
+public class CharacterWeaponTransform : MonoBehaviour
 {
     [SerializeField] private Transform _weapon;
 
