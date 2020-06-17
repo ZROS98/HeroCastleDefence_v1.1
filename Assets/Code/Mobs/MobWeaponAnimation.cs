f1b6303e7b89606fdb1ecae0bf6576abae21372f
Elenger <orion96king@gmail.com>
2020-06-17 22:11:35 +0200

    Fix MobWeaponAnimation

--- Assets/Code/Mobs/MobWeaponAnimation.cs
+++ Assets/Code/Mobs/MobWeaponAnimation.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MobAnimations : MonoBehaviour
+public class MobWeaponAnimation : MonoBehaviour
 {
     [SerializeField] private Transform _weapon;
 
