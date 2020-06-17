98ee88a99bfeada5435cd3766fb4bc10d1f8716e
ZROS <zros98@gmail.com>
2020-06-17 21:50:59 +0200

    update assaion animation

---
+++ Assets/Code/Characters/CharacterAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Rigidbody _rigidbody;
+    private bool _attackAnimationIsActive = false;
+    private Vector3 previousFrame;
+
+    private void SetAttackAnimationIsActive(int eventValue)
+    {
+        _attackAnimationIsActive = eventValue == 1 ? true : false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !_attackAnimationIsActive)
+        {
+            _animator.Play("SwordAttack");
+        }
+        else if (!_attackAnimationIsActive && previousFrame == transform.position)
+        {
+            _animator.Play("Idle");
+        }
+        else if (!_attackAnimationIsActive && previousFrame != transform.position)
+        {
+            _animator.Play("Run");
+        }
+        previousFrame = transform.position;
+    }
+}
