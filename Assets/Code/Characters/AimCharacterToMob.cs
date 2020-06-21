f763e746656e98ad2d0546181452454b8030c246
Elenger <orion96king@gmail.com>
2020-06-21 04:29:21 +0200

    fix AimCharacterToMob

--- Assets/Code/Characters/AimCharacterToMob.cs
+++ Assets/Code/Characters/AimCharacterToMob.cs
@@ -1,38 +1,21 @@
-﻿using Photon.Pun;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AimCharacterToMob : MonoBehaviour
 {
     [SerializeField] private int _numberLayerCharacter = 9;
     private const int maxRaycastDistance = 100;
-    private int _layerMask;
 
-    void Start()
-    {
-        // Bit shift the index of the layer (_numberLayerCharacter) to get a bit mask
-        _layerMask = 1 << _numberLayerCharacter;
-        // This would cast rays only against colliders in layer 9.
-        // But instead we want to collide against everything except layer 9. The ~ operator does this, it inverts a bitmask.
-        _layerMask = ~ _numberLayerCharacter;
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
         Ray rayToMob = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
 
-        //Mathf.Infinity
-        if (Physics.Raycast(rayToMob, out hit, 10000, _layerMask))
+        if (Physics.Raycast(rayToMob, out RaycastHit hit, Mathf.Infinity, _numberLayerCharacter))
         {
-            if (hit.collider.CompareTag("Mob"))
+            if (hit.transform.CompareTag("Mob"))
             {
                 Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                //PhotonNetwork.Destroy(hit.transform.gameObject);
                 Debug.Log(hit.transform.gameObject);
+                hit.transform.localScale = new Vector3(5, 5, 5);
             }
         }
     }
