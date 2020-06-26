9c6e6ffd3ce592c7cf1ddfc97839398d3bf1bca9
Elenger <orion96king@gmail.com>
2020-06-26 21:49:40 +0200

    fix event part of scripts

--- Assets/Code/Mobs/MobNavMesh.cs
+++ Assets/Code/Mobs/MobNavMesh.cs
@@ -12,6 +12,7 @@ public class MobNavMesh : MonoBehaviourPun
     private float _distanceDifference;
     private const int _agrOnPlayerDistance = 10;
     private bool _stopMovement = false;
+    private bool _targetIsDead = false;
     public Transform targetCharacterTransform;
     public Vector3 targetCastlePosition;
 
@@ -40,8 +41,13 @@ public class MobNavMesh : MonoBehaviourPun
         if (lifeStatus)
         {
             _navMeshAgent.SetDestination(targetCharacterTransform.position);
+            _targetIsDead = false;
         }
-        else _navMeshAgent.SetDestination(targetCastlePosition);
+        else
+        {
+            _navMeshAgent.SetDestination(targetCastlePosition);
+            _targetIsDead = true;
+        }
     }
 
     private void Update()
@@ -51,7 +57,7 @@ public class MobNavMesh : MonoBehaviourPun
         Vector3 targetCharacterPosition = targetCharacterTransform.position;
         _distanceDifference = Vector3.Distance(targetCharacterPosition, transform.position);
 
-        if (_distanceDifference <= _agrOnPlayerDistance)
+        if (_distanceDifference <= _agrOnPlayerDistance && !_targetIsDead)
         {
             _navMeshAgent.SetDestination(targetCharacterPosition);
         }
