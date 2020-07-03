ee5a03c3b9c40d12d07d0f6e64193f1e96a4593b
ZROS <zros98@gmail.com>
2020-07-03 22:45:20 +0200

    Fix Character rotation

---
+++ Assets/Code/Characters/CharacterAutoAim.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAutoAim : MonoBehaviour
+{
+    [SerializeField] private int _rotationSpeed = 5;
+    private void OnEnable()
+    {
+        EventManager.current.CharacterAimChanged += AimCharacterToMob;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.current.CharacterAimChanged -= AimCharacterToMob;
+    }
+
+    private void AimCharacterToMob(Transform mob)
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(mob.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+    }
+}
