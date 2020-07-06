63b370f8f74ed7ad006c1e0aa6fc9b5bc5c8c203
Elenger <orion96king@gmail.com>
2020-07-06 18:03:34 +0200

    Realized showing and hiding Shop and PressF panels. Shop panel will be open if during PressF panel show a player clicks "F". A bit changed raycasts

--- Assets/Code/BuildStatement/ShopInfo.cs
+++ Assets/Code/BuildStatement/ShopInfo.cs
@@ -6,21 +6,23 @@ public class ShopInfo : MonoBehaviour
 {
     [SerializeField] private ShopHighlight _shopHighlight;
     [SerializeField] private ShopAimCharacter _shopAimCharacter;
-    public int shopId;
+    [SerializeField] private PressFPanelAvailability _pressFPanelAvailability;
     private bool _shopInTarget = false;
+    public int shopId;
 
-    public void SetShopInTarget(bool shopInTarget, CharacterAutoAim characterAutoAim = null)
+    public void SetShopInTarget(bool shopInTarget, Vector3 characterPosition = new Vector3(), CharacterAutoAim characterAutoAim = null)
     {
         _shopInTarget = shopInTarget;
-
         if (_shopInTarget)
         {
             _shopHighlight.ShopInTarget(shopInTarget);
             _shopAimCharacter.CharacterAim(characterAutoAim);
+            _pressFPanelAvailability.ShowPressFPanel(shopInTarget, characterPosition);
         }
         else
         {
             _shopHighlight.ShopInTarget(shopInTarget);
+            _pressFPanelAvailability.ShowPressFPanel(shopInTarget);
         }
     }
 }
--- Assets/Code/BuildStatement/ShopInteraction.cs
+++ Assets/Code/BuildStatement/ShopInteraction.cs
@@ -10,6 +10,7 @@ public class ShopInteraction : MonoBehaviour
     [SerializeField] private PhotonView _photonView;
     [SerializeField] private CharacterAutoAim _characterAutoAim;
     [SerializeField] private int _distanceToShop = 5;
+    [SerializeField] private LayerMask _layerMask;
     private ShopInfo _shopInfo;
     private GameObject _previousObject;
     private int _previousShopID;
@@ -33,13 +34,13 @@ public class ShopInteraction : MonoBehaviour
 
         Debug.DrawRay(_rayPosition, cameraDirection * _distanceToShop, Color.blue);
 
-        if (Physics.Raycast(rayToMob, out RaycastHit hit, _distanceToShop))
+        if (Physics.Raycast(rayToMob, out RaycastHit hit, _distanceToShop, _layerMask))
         {
             GameObject hitObject = hit.transform.gameObject;
-            if (hitObject.CompareTag("Shop") && _previousObject == null)
+            if (_previousObject == null)
             {
                 _shopInfo = hitObject.GetComponent<ShopInfo>();
-                _shopInfo.SetShopInTarget(true, _characterAutoAim);
+                _shopInfo.SetShopInTarget(true, transform.position, _characterAutoAim);
                 _previousObject = hitObject;
             }
             else if (_previousObject != null && _previousObject != hitObject)
